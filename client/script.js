e9e2c18c02d8aec4bb9a685be74b1b88abfac20b
maximforever <flufci@gmail.com>
2019-03-08 11:15:07 -0500

    get access token from spotify API using Client Credentials Flow

--- client/script.js
+++ client/script.js
@@ -1,1 +1,25 @@
 console.log("hello world!");
+
+
+document.getElementById("auth").addEventListener("click", () => {
+	console.log("I can't believe you've done this");
+
+
+
+	let url = "/test"
+	let myData = JSON.stringify({
+		name: "max"
+	});
+
+
+	fetch(url, {
+		method: "POST",
+		body: myData,
+	})
+	  .then(function(response) {
+	    return response.json();
+	  })
+	  .then(function(myJson) {
+	    console.log(myJson);
+	  });
+})
--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let request = require('request');
+let router = express.Router();
+
 
 const bodyParser = require('body-parser');              // parse request body
 
@@ -10,18 +12,59 @@ require('dotenv').config()                              // access .env file
 
 /* log incoming request*/
 router.use(function(req, res, next){                                            // logs request URL
-    var timeNow = new Date();
+    let timeNow = new Date();
     console.log(`==> ${req.method.toUpperCase()} ${req.url} on ${timeNow}`);
     next();
 });
 
 /* ROUTES */
 
-router.get("/", function(req, res){
-
+router.get("/", (req, res) =>{
     res.render("index", {session: req.session});
     
 });
+
+router.post("/test", (req, res) => {
+
+    console.log("Got a post to test!");
+
+    let data = JSON.stringify({
+        
+    });
+
+    let requestUrl = "https://accounts.spotify.com/api/token";
+
+    var auth = "Basic " + new Buffer.from(process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET).toString('base64');
+
+    request.post({
+            uri: requestUrl,
+            headers: {
+                'Authorization': auth
+            },
+            form: {
+                grant_type: "client_credentials"
+            },
+            json: true
+        },
+        function(err, spotifyResponse, body){
+
+            if(err){
+                console.log("ERROR!");
+                console.log(err);
+            } else {
+
+                console.log("access token:");
+                console.log(body.access_token)
+
+                res.send({
+                    message:"yo, dis yo server. we cool."
+                })
+            }
+        })
+
+
+
+})
 
 /* 404 */
 
